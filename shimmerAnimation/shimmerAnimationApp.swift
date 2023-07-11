d4d0b29a96c744e23c32a906ef1b8e0a60de34b6
LBkos <constantin.l.o.p@gmail.com>
2023-07-11 17:11:18 +0700

    Initial Commit

---
+++ shimmerAnimation/shimmerAnimationApp.swift
@@ -0,0 +1,17 @@
+//
+//  shimmerAnimationApp.swift
+//  shimmerAnimation
+//
+//  Created by Константин Лопаткин on 11.07.2023.
+//
+
+import SwiftUI
+
+@main
+struct shimmerAnimationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
